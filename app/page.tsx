11369c93946d8dd459e8382f25196da48eef4063
eliudjgomezg <eliudjgomezg@gmail.com>
2023-03-04 02:13:09 -0300

    update package.json

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import Footer from 'components/Footer'
 
 export default function Home() {
-  console.log('hola mundo')
-
   return (
     <main className="flex h-screen items-center justify-center">
       <h1 className="border-2 border-green-400">Nextjs Template</h1>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
+  console.log('hola mundo')
   return <div>Footer</div>
 }
 
